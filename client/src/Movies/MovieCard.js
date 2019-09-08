56b7f6e6b501dd0feb646c9e0286fa8619924214
Robert Kulp <pedalinon@gmail.com>
2019-09-08 10:37:07 -0700

    Working on stretch

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const MovieCard = props => {
-  return;
+	const { title, director, metascore, stars, id } = movie;
+  return(
+	<div className="movie-card">
+
+    	<h2>{title}</h2>
+    	<div className="movie-director">
+      		Director: <em>{director}</em>
+    	</div>
+    	<div className="movie-metascore">
+      		Metascore: <strong>{metascore}</strong>
+    	</div>
+
+    	<h3>Actors</h3>
+    	<p>{id}</p>
+    	{stars.map(star => (
+      	<div key={star} className="movie-star">
+        	{star}
+      	</div>
+    ))}
+  </div>
+  	);
 };
 
 export default MovieCard;
